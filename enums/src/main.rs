b04490f7df6ac0af2f38215b956da2f9cd05280b
chris-eminence <emichris380@gmail.com>
2025-01-30 23:20:06 +0100

    Defining and Instantiating Structs

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,39 +1,42 @@
 fn main() {
-    
-    struct Spaceship {
-        name: String,
-        color: String,
-        engines: i32,
-    }
+    // Print a greeting message to the console
+    println!("Hello, world!");
 
-    let my_spaceship = Spaceship {
-        name: String::from("Star Blaster"),
-        color: String::from("red"),
-        engines: 2,
-    };
-
-    println!("Hello, world! {}", my_spaceship.name);
-
-
+    // Define an enum to represent the kind of IP address (either V4 or V6)
     enum IpAddrKind {
         V4,
         V6,
     }
 
+    // Define a struct to store an IP address along with its kind
     struct IpAddr {
         kind: IpAddrKind,
         address: String,
     }
 
+    // Create an instance of IpAddr for an IPv4 address
     let home = IpAddr {
         kind: IpAddrKind::V4,
         address: String::from("127.0.0.1"),
     };
 
+    // Create an instance of IpAddr for an IPv6 address
     let loopback = IpAddr {
         kind: IpAddrKind::V6,
         address: String::from("::1"),
     };
 
-}
+    // Define a generic enum to represent an optional value
+    enum Option<T> {
+        None,
+        Some(T),
+    }
 
+    // Create instances of Option with some values
+    let some_number = Some(5);
+    let some_char = Some('e');
+
+    // Create instances of Option with no values (None)
+    let absent_number: Option<i32> = None;
+    let absent_char: Option<char> = None;
+}
